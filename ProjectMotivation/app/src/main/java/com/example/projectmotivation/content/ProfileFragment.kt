f77d99ccea7d53a60fc1aabf0e04c0ba8f66a90a
Catalin-Andrei <andrei.cat10@gmail.com>
2022-09-29 12:29:58 +0300

    Switched to Real-Time DB Added 'Messages' Fragment (currently working on that)

--- ProjectMotivation/app/src/main/java/com/example/projectmotivation/content/ProfileFragment.kt
+++ ProjectMotivation/app/src/main/java/com/example/projectmotivation/content/ProfileFragment.kt
@@ -5,14 +5,32 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import androidx.navigation.fragment.findNavController
 import com.example.projectmotivation.R
+import com.example.projectmotivation.databinding.FragmentProfileBinding
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
 
 class ProfileFragment : Fragment() {
+
+    private lateinit var binding: FragmentProfileBinding
+    private lateinit var auth: FirebaseAuth
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
+        binding = FragmentProfileBinding.inflate(inflater,container,false)
+        auth = Firebase.auth
+        binding.btnLogout.setOnClickListener {
+            auth.signOut()
+            //takes you to the welcome screen (login,register)
+            findNavController().navigate(R.id.action_mainFragment_to_welcomeFragment)
+            Toast.makeText(context, "Logging you out!", Toast.LENGTH_LONG).show()
+        }
 
-        return inflater.inflate(R.layout.fragment_profile, container, false)
+        return binding.root
     }
 }
